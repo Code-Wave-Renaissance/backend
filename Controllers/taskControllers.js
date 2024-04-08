ea3b5aa762969bf73086b49eb18cb7f19da8e7b0
Daniel Yuki <danielyukihiga1@gmail.com>
2024-04-08 16:13:50 -0300

    addApplicant fixed for general cases

--- Controllers/taskControllers.js
+++ Controllers/taskControllers.js
@@ -84,7 +84,9 @@ export const updateTask = async (req, res, next) => {
 export const addApplicants = async (req, res, next) => {
     try {
         const id = req.params.id;
-        const newApplicant = req.body.applicant; // Assuming the request body contains the new applicant data
+        const newApplicant = req.body; // Assuming the request body contains the new applicant data
+
+        // console.log(newApplicant);
 
         // Get the existing task document
         const taskRef = doc(db, "tasks", id);
@@ -93,18 +95,11 @@ export const addApplicants = async (req, res, next) => {
         if (taskSnap.exists()) {
             // Get the existing applicants data
             const existingData = taskSnap.data().applicants || {};
-
-            // Generate a new key for the new applicant
-            const newApplicantKey = Date.now().toString();
-
-            // Construct the updated applicants object with the new applicant
-            const updatedApplicants = {
-                ...existingData,
-                [newApplicantKey]: newApplicant
-            };
+            
+            existingData.push(newApplicant);
 
             // Update the task document with the updated applicants object
-            await updateDoc(taskRef, { applicants: updatedApplicants });
+            await updateDoc(taskRef, { applicants: existingData});
 
             res.status(200).send("New applicant added successfully");
         } else {
